9986fbef0b34dcf6ae1e9646fa1b139d340534f5
jedojade <jade.ifurung@gmail.com>
2019-11-11 21:16:19 +0800

    Updates

---
+++ precache-manifest.967e72e552cefd769f03f4072eeda357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed459cda21fee2cdcf1850a545ec3513",
+    "url": "/where-to-eat/index.html"
+  },
+  {
+    "revision": "bda3c74b70be4405f65f",
+    "url": "/where-to-eat/static/css/main.4262a9d2.chunk.css"
+  },
+  {
+    "revision": "dfe51f5811cdb7f482db",
+    "url": "/where-to-eat/static/js/2.f913d141.chunk.js"
+  },
+  {
+    "revision": "bda3c74b70be4405f65f",
+    "url": "/where-to-eat/static/js/main.fa7424c1.chunk.js"
+  },
+  {
+    "revision": "5c118d783b249a860e6e",
+    "url": "/where-to-eat/static/js/runtime-main.7105e28b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/where-to-eat/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/where-to-eat/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
